143e3778c6ad0f17c4700007af3001122d5404b3
sharafadeen hammed <sharafadeenhammed@yahoo.com>
2026-01-19 22:57:26 +0100

    addded missing field

--- validators/organization.validators.ts
+++ validators/organization.validators.ts
@@ -3,9 +3,10 @@ import { CreateOrganizationType } from "../models/organizations.model.js";
 
 
 const createOrganizationValidator = Joi.object<CreateOrganizationType>({
-  company_name: Joi.string().required().min(2),
+  company_name: Joi.string().required().min(2).required(),
   email: Joi.string().email().required(),
-  company_size: Joi.string().valid("1-10", "11-50", "50+")
+  company_size: Joi.string().valid("1-10", "11-50", "50+").required(),
+  industry: Joi.string().required(),
 });
 
 const OrganizationValidators = {
